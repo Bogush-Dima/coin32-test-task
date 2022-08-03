53619f9f465143841e86e00563ed329378eb00fe
Dzmitry-Bohush <dzmitry.bohush@innowise-group.com>
2022-08-03 10:39:48 +0300

    feat: completed adaptive game info page

--- pages/components/button/styled.js
+++ pages/components/button/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../../constants/devicesSizes";
 
 export const StyledButton = styled.button`
   background: #7dd235;
@@ -8,4 +9,14 @@ export const StyledButton = styled.button`
   font-weight: 700;
   border-radius: 40px;
   cursor: pointer;
+
+  &.visitWebsite {
+    margin-top: 20px;
+    margin-left: 30px;
+  };
+
+  @media ${device.tablet} {
+    padding: 5px 10px;
+    font-size: 15px;
+  }
 `
--- pages/components/gameInfo/styled.js
+++ pages/components/gameInfo/styled.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { device } from "../../constants/devicesSizes";
 
 export const DescriptionStyled = styled.div`
   font-size: 24px;
   font-weight: 500;
   padding: 0 30px;
   margin: 40px 0;
+
+  @media ${device.tablet} {
+    font-size: 15px;
+  }
 `
